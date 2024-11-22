109b73612add94808106fd0b4af62ec3706853d4
Guilherme Oliveira <guilherme.olbrs34@hotmail.com>
2024-11-22 00:36:33 -0300

    feat(p2-pdmn):Adiciona botão principal e FlatList

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {FlatList, Pressable, StyleSheet, Text,  View }from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Pressable style={styles.button}>
+          <Text  style={styles.buttonText}>
+            { 
+              'Adicionar gato'
+            }
+            </Text>
+      </Pressable>
+      <FlatList style={styles.list}> 
+        <View style={styles.listItem}>
+          <Text style={styles.listemItemText}>
+            
+          </Text>
+        </View >
+      </FlatList>
     </View>
   );
 }
@@ -16,5 +28,39 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 40
   },
+  button:{
+    width: '70%',
+    backgroundColor:'#0096F3',
+    padding: 14,
+    borderRadius: 4,
+  },
+  buttonText:{
+    color: 'white',
+    textAlign: 'center'
+  },
+  list:{
+    borderWidth: 1,
+    borderColor:'gray',
+    width:'70%',
+    borderRadius:4,
+    marginTop: 12,
+    padding: 8,
+  },
+  listItem:{
+    padding:12,
+    backgroundColor: '#f0f0f0',
+    borderBottomWidth: 1,
+    borderBlockColor: '#CCC',
+    marginBottom: 4,
+    borderRadius:4,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  listemItemText:{
+    textAlign: 'center',
+    width: '70%',
+  }
+  
 });
